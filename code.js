fdb8df93da0f04a3e0893fa48ab02045a6c1ab68
supriyomaji <akashmaji200680@gmail.com>
2023-12-27 11:56:36 +0530

    first commit

--- code.js
+++ code.js
@@ -1,67 +1,11 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    document.getElementById('submitBtn').innerHTML = 'Loading...';
+    document.getElementById('submitBtn').disabled = true;
 
-//jQuery for Task-2 ATG internship Author : Bhupesh Prajapati
-$(document).ready(function () {
-    //tooltip
-    $('.fa-info').tooltip({title: "This is a tooltip. Use it to show if there is any help text.", placement: "top"});
-    //initial div3 selected
-    $("#div3").addClass("selected-box");
-    $("#nb").off("mouesehover");
-    $('input[type=radio][name=choice]').on('change', function () {
-        e=$(this).val();
-        e2='null';
-        //back button functioning
-        $("#backButton").on("click", function () {
-            $('#modal2').modal('hide');
-        if ($(this).val() == 'paid') {
-            $("#div1").addClass("selected-box");
-        }
-        if ($(this).val() == 'free') {
-            $("#div2").addClass("selected-box");
-        }
-        });
-        $("#backButton").on("click", function () {
-            $('#payment').modal('show');
-        });
-        $("#nb").removeClass("disabled"); //enabling the next button
-        //select box blue highlight for payment modal
-        if ($(this).val() == 'paid') {
-            $("#div1").addClass("selected-box");
-        }
-        else {
-            $("#div1").removeClass("selected-box");
-        }
-        if ($(this).val() == 'free') {
-            $("#div2").addClass("selected-box");
-        }
-        else {
-            $("#div2").removeClass("selected-box");
-        }
-        //if paid is selected then next button leads to payed event modal
-        $("#nb").on("click", function () {
-            if (e == 'paid') {
-                $('#payment').modal('hide');
-                $('#modal2').modal('show');
-            }
-        });
-    });
-        //Modal 2 Seleletion logic
-    $('input[type=radio][name=choice2]').on('change', function () { 
-        e2=$(this).val();
-        //2nd modal
-        if ($(this).val() == 'scost') {
-            $("#div3").addClass("selected-box");
-        }
-        else {
-            $("#div3").removeClass("selected-box");
-        }
-        if ($(this).val() == 'mcost') {
-            $("#div4").addClass("selected-box");
-        }
-        else {
-            $("#div4").removeClass("selected-box");
-        }
-        if($(this).val()!='mcost'){
-                $("#div3").addClass("selected-box");
-            }
-    });
+    // Simulate a 2-second loading state
+    setTimeout(function() {
+        document.getElementById('submitBtn').innerHTML = 'Submit';
+        document.getElementById('submitBtn').disabled = false;
+    }, 2000);
 });
